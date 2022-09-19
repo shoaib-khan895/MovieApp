8dbe037c4440ebacbde4eaa6b11181912eabeda5
shoaib-khan895 <71784547+shoaib-khan895@users.noreply.github.com>
2022-09-19 15:03:29 +0530

    drawer left

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Home'
-import Constants from './src/Constants'
-import Icon from 'react-native-vector-icons/Ionicons'
-import DiscoverMovie from './src/Components/DiscoverMovie';
-
-
-
+import Home from './src/Home';
+import Constants from './src/Constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MovieDetails from './src/Components/MoviesDetails';
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -17,18 +12,24 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerTitleAlign: 'center' }}>
         <Stack.Screen name="Home" component={Home} options={headerStyle} />
-        {/* <Stack.Screen name="DiscoverMovie" component={DiscoverMovie} options={headerStyle} /> */}
+        <Stack.Screen
+          name="movieDetails"
+          component={MovieDetails}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
 const headerStyle = {
-  title: ' Movies ',
+  title: 'Movies',
   headerStyle: { backgroundColor: Constants.baseColor },
   headerTitleStyle: { color: Constants.textColor },
-  headerLeft: () => <Icon name="menu" size={28} color=
-    {Constants.textColor} />,
-  headerRight: () => <Icon name="search" size={28} color=
-    {Constants.textColor}></Icon>,
+  headerLeft: () => <Icon name="menu" size={34} color={Constants.textColor} />,
+  headerRight: () => (
+    <Icon name="search" size={25} color={Constants.textColor} />
+  ),
 };
+
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,22 @@
-import * as React from 'react';
-import { View, Text } from 'react-native'
-import DiscoverMovie from './Components/DiscoverMovie';
+import React from 'react';
+import {View} from 'react-native';
+import DiscoverMovies from './Components/DiscoverMovies';
+import Styles from './Styles';
+import TrendingPeople from './Components/TrendingPeople';
+import TrendingMovies from './Components/TrendingMovies';
 
-const Home = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <DiscoverMovie/>
-        </View>
-    );
-}
+const Home = props => {
+  return (
+    <View style={Styles.sectionBg}>
+      <DiscoverMovies navigation={props.navigation} />
+      <TrendingPeople title="Trending People" url="/trending/person/week" />
+      <TrendingMovies
+        title="Trending Movies"
+        url="/movie/top_rated"
+        navigation={props.navigation}
+      />
+    </View>
+  );
+};
+
 export default Home;
